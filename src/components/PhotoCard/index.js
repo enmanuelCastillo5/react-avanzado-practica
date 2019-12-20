a0d6c5cf808fc4b14c03b71a89038f98a9dc5a5c
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2019-12-20 15:39:36 -0400

    agregando useState y useEffect

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { ImgWrapper, Img, Button } from './styles'
 
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder } from 'react-icons/md';
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
 
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -12,7 +12,7 @@ width: 100%;
 `
 
 export const Img = styled.img`
-${fadeIn({time: '1.5s'})}
+${fadeIn({ time: '1s' })}
 box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
 height: 100%;
 object-fit: cover;
