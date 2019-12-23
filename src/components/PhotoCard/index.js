ec9bd3d772b6841aeda13a3a06fe7e8e883f7dd6
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2019-12-23 19:35:05 -0400

    custom Hooks: useNearScreen and useLocalStorage

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -4,48 +4,20 @@ import { Article, ImgWrapper, Img, Button } from './styles'
 
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+
+import { useNearScreen } from '../../hooks/useNearScreen'
+
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_cats.jpg'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  const [show, element] = useNearScreen()
   const key = `like-${id}`
-  const [liked, setLiked] = useState(() => {
-    try {
-      const like = window.localStorage.getItem(key)
-      return JSON.parse(like)
-    } catch (error) {
-      return false
-    }
-  })
+  const [liked, setLiked] = useLocalStorage(key, false)
 
   console.log(liked)
 
-  useEffect(function () {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined' ? window.IntersectionObserver : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(element.current)
-    })
-  }, [element])
-
   const Icon = liked ? MdFavorite : MdFavoriteBorder
-
-  const setLocalStorage = value => {
-    try {
-      window.localStorage.setItem(key, value)
-      setLiked(value)
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   return (
     <Article ref={element}>
@@ -57,11 +29,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button onClick={() => setLocalStorage(!liked)}>
+          <Button onClick={() => setLiked(!liked)}>
             <Icon size='32px' /> {likes} likes!
           </Button>
         </>
       }
     </Article>
   )
-}
+} 
