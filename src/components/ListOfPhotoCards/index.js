5c85c14da7a8f3033fd560fc4bc74093d66ca752
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2019-12-24 10:17:17 -0400

    imicializaando react-apollo-client

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -2,10 +2,30 @@ import React from 'react'
 
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCards = () => {
+import { graphql } from 'react-apollo'
+
+import { gql } from 'apollo-boost'
+
+const withPhoto = graphql(gql`
+query getPhotos {
+  photos {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`)
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+  console.log()
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)}
+      {photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
     </ul>
   )
 }
+
+export const ListOfPhotoCards = withPhoto(ListOfPhotoCardsComponent)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
+
 import ReactDom from 'react-dom'
+
+import ApolloClient from 'apollo-boost'
+
+import { ApolloProvider } from 'react-apollo'
 
 import { App } from './App.js'
 
-ReactDom.render(<App />,
+const client = new ApolloClient({
+  uri: 'https://petgram-server-enmanuelcast.enmanuelcastillo.now.sh/graphql'
+})
+
+ReactDom.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('app'))
