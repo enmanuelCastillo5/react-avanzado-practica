02695334fceef531d92c60eaf28e4f67009509d4
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2019-12-24 12:10:08 -0400

    paramatros para un query graphQl

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -2,24 +2,7 @@ import React from 'react'
 
 import { PhotoCard } from '../PhotoCard'
 
-import { graphql } from 'react-apollo'
-
-import { gql } from 'apollo-boost'
-
-const withPhoto = graphql(gql`
-query getPhotos {
-  photos {
-    id
-    categoryId
-    src
-    likes
-    userId
-    liked
-  }
-}
-`)
-
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   console.log()
   return (
     <ul>
@@ -27,5 +10,3 @@ const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
     </ul>
   )
 }
-
-export const ListOfPhotoCards = withPhoto(ListOfPhotoCardsComponent)
