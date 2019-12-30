62ec31978e8d6d0d00faa3badf8e2c64dcc58837
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2019-12-30 12:46:35 -0400

    protegiendo rutas para usuarios no registrados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
+
 import Logo from './components/Logo'
 import { NavBar } from './components/NavBar'
-import { Home } from './pages/Home'
-import { Router } from '@reach/router'
-import { Detail } from './pages/Detail'
 
+import { Router } from '@reach/router'
+
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 export const App = () => {
 /*   const urlParams = new window.URLSearchParams(window.location.search)
   const detailId = urlParams.get('detail') */
@@ -19,6 +28,23 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              :
+              <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+
+      </UserLogged>
       <NavBar />
     </div>
   )
