2ae9423fc77e64a257bc480d10b953553f47070c
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2020-01-06 20:30:38 -0400

    cypress y concluyendo proyecto

---
+++ cypress/integration/petgram/test_spec.js
@@ -0,0 +1,24 @@
+/* global describe, it, cy */
+
+describe('PetGram', function () {
+  it('para ver si la App funciona', function () {
+    cy.visit('/')
+  })
+
+  it('navegamos a una categoria y vemos fotos', function () {
+    cy.visit('/pet/1')
+    cy.get('article')
+  })
+
+  it('si podemos navegar con la navbar a la home', function () {
+    cy.visit('/pet/1')
+
+    cy.get('nav a').first().click()
+    cy.url().should('include', '/')
+  })
+
+  it('los usuarios no registrados ven el formulario de registro e inicio de seccion al ir a Favs', function () {
+    cy.visit('/favs')
+    cy.get('form').should('have.length', 2)
+  })
+})
